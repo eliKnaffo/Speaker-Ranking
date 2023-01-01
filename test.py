7b58002d7809f349e246e0f91fd7d2be3ac42e3b
eliknaffo <eli.knaffo@gmail.com>
2023-01-01 17:19:25 +0200

    Fixed devider function and removed unused code

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 import pprint
 import numpy as np
 from pydub import AudioSegment
+import speech_recognition as sr
 from collections import namedtuple
 from collections import defaultdict
 from pyannote.audio import Pipeline
@@ -9,6 +10,7 @@ from pyannote.audio import Pipeline
 
 Splits = namedtuple('Splits',('start','stop'))
 speakers_dict = defaultdict(list)
+speaker_ranking_list = []
 
 
 
@@ -19,16 +21,16 @@ pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
 
 def devide_wav_by_speaker_and_intervals(wav_file, speaker_id):
     myaudio = AudioSegment.from_file(wav_file, "wav")
-    intervals_list = []
     
-    for curr_speaker,Splits in speakers_dict.items():
-      print(curr_speaker)
-      if(curr_speaker == speaker_id):
-        print(Splits.start)
-        wav_file_part = myaudio[Splits.start:Splits.end]
-        intervals_list.append(wav_file_part)
+    for curr_speaker,curr_split_list in speakers_dict.items():
+      
+      if(int(curr_speaker) == speaker_id):     
+        for curr_split in curr_split_list:
+          wav_file_part = myaudio[curr_split.start : curr_split.stop]
+          #need to add formating so the export is dynamic
+          wav_file_part.export( "first_speaker/audio_chunk_{}.wav".format(curr_split.stop), format="wav")      
     
-    return intervals_list
+    
       
 
 
@@ -50,72 +52,30 @@ def speaker_diaraztion():
   print("done!")
 
 
+#needs to be in a for loop probably
+def get_speech_rate(interval):
+    # Load the WAV file
+    r = sr.Recognizer()
+    with sr.AudioFile(interval) as source:
+        audio = r.record(source)
+    # Calculate the speech rate
+    rate = sr.speech_rate(audio)
+    return rate
 
-def avg_speed_of_speaker(speaker_id):
-  #get speaker intervals
-  speaker_intervals = speakers_dict.get(speaker_id)
-  #check speed in each interval
-  #avarge the intervals
-  #save to speaker speeds dict
-  print(speaker_intervals)
-  
-  
+
+def speaker_rank_list_init():
+    for speaker_id in len(speakers_dict.keys()):
+      #init the ranking score to 0
+      speaker_ranking_list[speaker_id] = 0
+ 
+ 
+#need to add next -> format how the files are saved -> how to iterate over those files by speaker for each feature.  
 def main():
     speaker_diaraztion()
-    test_list = devide_wav_by_speaker_and_intervals("test.wav",1)
-    
-    print(test_list)
+    speaker_rank_list_init()
+    for speaker_id in len(speakers_dict.keys()):
+      devide_wav_by_speaker_and_intervals("test.wav",speaker_id)
     
     
     
 main()
-
-"""
-# Create an array to hold the pitch of each speaker's speech
-speaker_pitches = []
-for i in range(n_speakers):
-  speaker_pitches.append(0)
-
-# Calculate the pitch of each speaker's speech
-for i, cluster_id in enumerate(cluster_ids):
-  # Calculate the pitch of speech as the average frequency of the audio segment
-  pitch = np.mean(librosa.pitch_tuning(audio[i]))
-  speaker_pitches[cluster_id] += pitch
-
-# Create an array to hold the repetitiveness of each speaker's speech
-speaker_repetitiveness = []
-for i in range(n_speakers):
-  speaker_repetitiveness.append(0)
-
-# Calculate the repetitiveness of each speaker's speech
-for i, cluster_id in enumerate(cluster_ids):
-  # Calculate the repetitiveness of speech as the average repetition rate of the audio segment
-  repetitiveness = np.mean(librosa.beat.tempo(audio[i], sr=sr))
-  speaker_repetitiveness[cluster_id] += repetitiveness
-
-# Create an array to hold the pauses of each speaker's speech
-speaker_pauses = []
-for i in range(n_speakers):
-  speaker_pauses.append(0)
-
-# Calculate the pauses of each speaker's speech
-for i, cluster_id in enumerate(cluster_ids):
-  # Calculate the pauses in speech as the number of silence segments in the audio
-  pauses = len(librosa.effects.split(audio[i], top_db=20))
-  speaker_pauses[cluster_id] += pauses
-
-# Create an array to hold the scores for each speaker
-speaker_scores = []
-for i in range(n_speakers):
-  speaker_scores.append(0)
-
-# Calculate the overall score for each speaker
-for i in range(n_speakers):
-  score = speaker_speeds[i] + speaker_pitches[i] - speaker_repetitiveness[i] - speaker_pauses[i]
-  speaker_scores[i] = score
-
-
-
-print(speaker_scores)
-
-"""
