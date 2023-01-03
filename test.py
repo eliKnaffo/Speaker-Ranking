8f48eaf74536fbb9a3e37e11bf36d6aaddb26598
eliknaffo <eli.knaffo@gmail.com>
2023-01-03 19:50:08 +0200

    multiple fixes and added support for multi wav files

--- test.py
+++ test.py
@@ -7,33 +7,34 @@ from collections import namedtuple
 from collections import defaultdict
 from pyannote.audio import Pipeline
 
+r = sr.Recognizer()
 
 Splits = namedtuple('Splits',('start','stop'))
 speakers_dict = defaultdict(list)
-speaker_ranking_list = []
-
+speaker_ranking_list = [0] * 10
+repeat_ranking_list = [0] * 10
+speed_ranking_list = [0] * 10
+pitch_ranking_list = [0] * 10
+pasues_ranking_list = [0] * 10
 
 
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
-                                    use_auth_token="")
+                                    use_auth_token="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")
 
 
 
-def devide_wav_by_speaker_and_intervals(wav_file, speaker_id):
+def devide_wav_by_speaker_and_intervals(wav_file):
     myaudio = AudioSegment.from_file(wav_file, "wav")
     
-    for curr_speaker,curr_split_list in speakers_dict.items():
-      
-      if(int(curr_speaker) == speaker_id):     
+    for curr_speaker,curr_split_list in speakers_dict.items():     
         for curr_split in curr_split_list:
           wav_file_part = myaudio[curr_split.start : curr_split.stop]
-          #need to add formating so the export is dynamic
-          wav_file_part.export( "first_speaker/audio_chunk_{}.wav".format(curr_split.stop), format="wav")      
+          
+          if not os.path.exists("speaker_{speaker_id}".format(speaker_id = int(curr_speaker))):
+            os.makedirs("speaker_{speaker_id}".format(speaker_id = int(curr_speaker)))
+          
+          wav_file_part.export( "speaker_{speaker_id}/audio_chunk_{curr_interval}.wav".format(curr_interval = curr_split.stop, speaker_id = int(curr_speaker)), format="wav")      
     
-    
-      
-
-
 
 def split_speaker_id(speaker):
   return speaker[-1]
@@ -53,29 +54,62 @@ def speaker_diaraztion():
 
 
 #needs to be in a for loop probably
-def get_speech_rate(interval):
+def get_speech_rate(file):
     # Load the WAV file
     r = sr.Recognizer()
-    with sr.AudioFile(interval) as source:
+    with sr.AudioFile(file) as source:
         audio = r.record(source)
     # Calculate the speech rate
     rate = sr.speech_rate(audio)
-    return rate
+    
+    #add the avarge calculation
+    print(rate)
 
 
-def speaker_rank_list_init():
-    for speaker_id in len(speakers_dict.keys()):
-      #init the ranking score to 0
-      speaker_ranking_list[speaker_id] = 0
+def repetative_analsys(file, speaker_id):
+  
+  text = r.recognize_google(file)
+  total_repeat_sum = 0
+  
+  splited_text = text.split()
+  #a dict to hold how much each word repeats
+  word_counts = {}
+  
+  for word in splited_text:
+     # If the word is not yet in the dictionary, add it with a count of 1
+    if word not in word_counts:
+      word_counts[word] = 1
+    # If the word is already in the dictionary, increment its count
+    else:
+      word_counts[word] += 1
+  
+  for curr_value in word_counts.values():
+    if curr_value >= 3:
+      total_repeat_sum += 1
+  
+  if(total_repeat_sum >= 3):
+    repeat_ranking_list[speaker_id] = -1
+  else:
+    repeat_ranking_list[speaker_id] = 1
+  
+  
+
  
  
 #need to add next -> format how the files are saved -> how to iterate over those files by speaker for each feature.  
 def main():
-    speaker_diaraztion()
-    speaker_rank_list_init()
-    for speaker_id in len(speakers_dict.keys()):
-      devide_wav_by_speaker_and_intervals("test.wav",speaker_id)
-    
+  speaker_diaraztion()
+  devide_wav_by_speaker_and_intervals("test.wav")
+
+  for curr_speaker in speakers_dict.keys():
+    print("im in the first for loop")
+    for filename in os.listdir("speaker_{speaker_id}".format(speaker_id = int(curr_speaker))):
+      f = os.path.join("speaker_{speaker_id}".format(speaker_id = int(curr_speaker)),filename)
+      print("im before the if")
+      if os.path.isfile(f):
+        print("im after the if")
+        repetative_analsys(filename,curr_speaker)
+
     
     
 main()
